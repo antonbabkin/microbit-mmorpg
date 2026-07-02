9b5123e8484411713bec4aa93af67037d4e74d82
Anton Babkin <6345499+antonbabkin@users.noreply.github.com>
2026-07-01 19:44:54 -0500

    Add flashing food to eat and win

--- README.md
+++ README.md
@@ -11,3 +11,10 @@ Aspiring to build a multi-player game that uses radio to communicate between dev
 
 3. Install `mpremote` and use it to interactively run scripts from RAM on the microbit (saves flash write cycles) and copy programs (without changing the micropython runtime).
 
+
+```bash
+# run script in RAM
+mpremote run eat_the_dot.py
+# script to be main executable
+mpremote fs cp eat_the_dot.py :main.py
+```
--- eat-the-dot.py
+++ eat-the-dot.py
@@ -1,26 +1,37 @@
 
-from microbit import *
+from random import randint
 
-class Mob:
-    def __init__(self, x, y):
+from microbit import *
+import music
+
+FF_FANFARE = ["c5:1", "c5:1", "c5:1", "c5:3", "ab4:3", "bb4:3", "c5:2", "bb4:1", "c5:9"]
+
+class Sprite:
+    def __init__(self, x, y, brightness=9):
         self.x = x
         self.y = y
-        self.last_moved = running_time()
+        self.brightness = brightness
 
     def clear(self):
         display.set_pixel(self.x, self.y, 0)
 
     def show(self):
-        display.set_pixel(self.x, self.y, 5)
+        display.set_pixel(self.x, self.y, self.brightness)
+
+class Mob(Sprite):
+    def __init__(self, x, y):
+        super().__init__(x, y, 1)
+        self.last_moved = running_time()
 
     def input_move(self):
-        if accelerometer.get_x() < -300:
+        tilt_threshold = 250
+        if accelerometer.get_x() < -tilt_threshold:
             self.move("w")
-        elif accelerometer.get_x() > 300:
+        elif accelerometer.get_x() > tilt_threshold:
             self.move("e")
-        elif accelerometer.get_y() < -300:
+        elif accelerometer.get_y() < -tilt_threshold:
             self.move("n")
-        elif accelerometer.get_y() > 300:
+        elif accelerometer.get_y() > tilt_threshold:
             self.move("s")
         
     def move(self, dir):
@@ -39,16 +50,54 @@ class Mob:
             y1 = y0 + 1
 
         if x1 != x0 or y1 != y0:
+            if x1 == food.x and y1 == food.y:
+                audio.play(Sound.HAPPY, wait=True)
+                self.brightness = min(9, self.brightness + 1)
+                while food.x == x1 and food.y == y1:
+                    food.jump()
+                food.show()
+            
             self.last_moved = now
             self.clear()
             self.x = x1
             self.y = y1
             self.show()
-        
+
+
+class Food(Sprite):
+    def __init__(self):
+        super().__init__(randint(0, 4), randint(0, 4), 1)
+        self.brighten = True
+
+    def jump(self):
+        self.x = randint(0, 4)
+        self.y = randint(0, 4)
+
+    def update(self):
+        if self.brighten:
+            self.brightness += 1
+            if self.brightness >= 5:
+                self.brighten = False
+        else:
+            self.brightness -= 1
+            if self.brightness <= 1:
+                self.brighten = True
+        self.show()
+
 
 hero = Mob(2, 2)
 hero.show()
 
+food = Food()
+while food.x == hero.x and food.y == hero.y:
+    food.jump()
+food.show()
+
 while True:
     sleep(100)
     hero.input_move()
+    food.update()
+    if hero.brightness >= 9:
+        music.play(FF_FANFARE, wait=False)
+        display.show(Image.HAPPY, loop=True)
+
